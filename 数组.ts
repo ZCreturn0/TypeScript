05296a315c2d9870c44ddf3691adbd0c661b11a2
ZCreturn0 <237350543@qq.com>
2019-10-10 19:33:23 +0800

    接口描述数组

--- 数组.ts
+++ 数组.ts
@@ -6,3 +6,9 @@ arr.push('aaa');
 // 泛型定义数组
 let arr2: Array<number> = [1,2,3,4,5];
 console.log(arr2);
+
+// 用接口表示数组
+interface NumberArray{
+    [index: number]: string
+}
+let str: NumberArray = ['aaa','bbb','ccc'];
