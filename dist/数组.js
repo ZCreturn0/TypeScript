3b97d72f8ad8c44331f998e104b1cac0004806c3
ZCreturn0 <237350543@qq.com>
2019-10-15 19:28:18 +0800

    数组的基本用法

---
+++ dist/数组.js
@@ -0,0 +1,8 @@
+"use strict";
+// 数据类型必须与数组类型一致
+var arr = [1, 2, 3, 4, 5, 6];
+// 对数组方法进行限制
+arr.push('aaa');
+// 泛型定义数组
+var arr2 = [1, 2, 3, 4, 5];
+console.log(arr2);
---
+++ 数组.ts
@@ -0,0 +1,8 @@
+// 数据类型必须与数组类型一致
+let arr: number[] = [1,2,3,4,5,6];
+// 对数组方法进行限制
+arr.push('aaa');
+
+// 泛型定义数组
+let arr2: Array<number> = [1,2,3,4,5];
+console.log(arr2);
