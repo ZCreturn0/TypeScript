2a28dfeab5ccb8b698475001a8b00dd4aa807b62
zc_return0 <237350543@qq.com>
2019-10-17 17:54:41 +0800

    类型断言

---
+++ 类型断言.ts
@@ -0,0 +1,12 @@
+function getLength(param: number | string): number{
+    // 对类型断言
+    if(<string>param){
+        // 要调用 length 属性,必须断言,否则不能调用(number 类型没 length 属性)
+        return (<string>param).length;
+    }
+    // 另一种断言方式
+    else if((param as number)){
+        return (param as number);
+    }
+    return 0;
+}
