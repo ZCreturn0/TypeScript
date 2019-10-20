5c46a5ae9358e9d0e31e9c2fc14cb4a3a84122a3
ZCreturn0 <237350543@qq.com>
2019-10-20 22:36:47 +0800

    getter,setter,防溢出

--- 类.ts
+++ 类.ts
@@ -1,17 +1,26 @@
 class Animal{
+    protected name: string;
     constructor(name){
         this.name = name;
     }
     syaHi(){
         console.log(this.name);
     }
-    get name(){
+    // 静态方法
+    static isAnimal(a){
+        return a instanceof Animal;
+    }
+    get _name(){
         return this.name;
     }
-    set name(name){
+    // 给 name 属性赋值会调用这个函数
+    // setter 里又给 name 赋值
+    // 所以会无限调用,导致栈溢出
+    set _name(name){
         this.name = name;
     }
 }
+let a = new Animal('cccc');
 
 class Cat extends Animal{
     constructor(name){
@@ -24,3 +33,5 @@ class Cat extends Animal{
 
 let c = new Cat('aaaa');
 c.sayHi();
+console.log(Animal.isAnimal(a));
+console.log(Animal.isAnimal(c));
