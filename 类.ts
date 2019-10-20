75d1dc65f0483954dfccd91574117ce9c50dda49
ZCreturn0 <237350543@qq.com>
2019-10-20 22:03:17 +0800

    继承

--- 类.ts
+++ 类.ts
@@ -1,9 +1,18 @@
 class Animal{
-    private name: string;
+    protected name: string;
     constructor(name){
         this.name = name;
     }
     syaHi(){
         console.log(this.name);
+    }
+}
+
+class Cat extends Animal{
+    constructor(name){
+        super(name);
+    }
+    sayHi(){
+        console.log('Meow,'+this.name);
     }
 }
