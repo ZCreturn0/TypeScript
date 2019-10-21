eec0cc54664ae878685971358c81c19fe8b1cddf
zc_return0 <237350543@qq.com>
2019-10-21 09:21:59 +0800

    抽象类与抽象方法

--- 类.ts
+++ 类.ts
@@ -51,3 +51,27 @@ class Person{
 }
 // 报错,新建对象要访问构造函数,但构造函数被 protected 修饰,只能在子类访问
 // let p = new Person('aaaa');
+
+// 抽象类
+// 抽象类不能创建实例,只能被子类继承,且子类必须实现抽象类里面的抽象方法
+abstract class Fruit{
+    protected readonly name: string;
+    constructor(name){
+        this.name = name;
+    }
+    // 抽象方法不能写实现
+    public abstract taste();
+}
+
+class Apple extends Fruit{
+    constructor(name){
+        super(name);
+    }
+    // 在子类实现抽象方法
+    public taste(){
+        console.log('sweet');
+    }
+}
+
+let apple = new Apple('apple');
+apple.taste();
