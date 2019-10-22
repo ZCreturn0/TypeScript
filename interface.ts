b8f9c5d7f12f389bfbcdae572a0cab4e3b51ff30
zc_return0 <237350543@qq.com>
2019-10-22 17:26:20 +0800

    混合模式

--- interface.ts
+++ interface.ts
@@ -1,30 +1,30 @@
 // 接口:对公共方法,属性进行抽象
 // 接口里的方法都是抽象方法,实现接口的类都必须实现接口里的方法
 // 抽象告警
-interface Alarm{
+interface Alarm {
     alert(): void;
 }
 // 抽象声光告警
-interface LightAlarm{
+interface LightAlarm {
     lightAlarm(): void;
 }
 // 接口可多继承,接口可继承接口
-interface CarAlarm extends Alarm, LightAlarm{
+interface CarAlarm extends Alarm, LightAlarm {
     alert(): void;
     lightAlarm(): void;
 }
-class Door{}
-class SecurityDoor extends Door implements Alarm{
-    alert(): void{
+class Door { }
+class SecurityDoor extends Door implements Alarm {
+    alert(): void {
         console.log('SecurityDoor alarm');
     }
 }
 // 也可写成 class Car implements Alarm, LightAlarm,接口可多实现
-class Car implements CarAlarm{
-    lightAlarm(): void{
+class Car implements CarAlarm {
+    lightAlarm(): void {
         console.log('Lights toggle');
     }
-    alert(): void{
+    alert(): void {
         console.log('Car alarm');
         this.lightAlarm();
     }
@@ -35,3 +35,37 @@ s.alert();
 
 let c: Car = new Car();
 c.alert();
+
+// 定义函数自身的属性方法
+interface Counter {
+    (start: number): string,
+    interval: number,
+    current: number,
+    reset(): void,
+    log(): void,
+    add(): void
+}
+function getCounter(): Counter {
+    let counter = <Counter>function (start: number) {
+        console.log(start);
+    };
+    counter.interval = 1;
+    counter.current = 0;
+    counter.reset = function (): void {
+        this.current = 0;
+    };
+    counter.log = function (): void {
+        console.log(this.current);
+    }
+    counter.add = function (): void {
+        this.current += this.interval;
+    }
+    return counter;
+}
+let counter: Counter = getCounter();
+counter(10);
+counter.interval = 2;
+counter.log();
+counter.reset();
+counter.add();
+counter.log();
