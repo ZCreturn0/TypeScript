fab84c07aa1afce2f3ded6333377565f1041dec4
zc_return0 <237350543@qq.com>
2019-10-23 16:35:16 +0800

    泛型基本用法

---
+++ 泛型.ts
@@ -0,0 +1,21 @@
+// 生成任意类型的数组
+function createArray(length: number, value: any): Array<any> {
+    let arr = new Array<any>();
+    for (let i: number = 9; i < length; i++) {
+        arr.push(value);
+    }
+    return arr;
+}
+
+// 使用泛型约束类型
+function createTypeArray<T>(length: number, value: T): Array<T>{
+    let arr: T[] = [];
+    for (let i: number = 0; i < length; i++){
+        arr.push(value);
+    }
+    return arr;
+}
+let numArr = createTypeArray(3, 5);
+let strArr = createTypeArray(4, 'a');
+console.log(numArr);
+console.log(strArr);
