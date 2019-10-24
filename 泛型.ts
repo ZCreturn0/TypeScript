59d84632c34df4d4df431a54bb5f8f3bfa226426
zc_return0 <237350543@qq.com>
2019-10-24 17:53:25 +0800

    优化

--- 泛型.ts
+++ 泛型.ts
@@ -8,9 +8,9 @@ function createArray(length: number, value: any): Array<any> {
 }
 
 // 使用泛型约束类型
-function createTypeArray<T>(length: number, value: T): Array<T>{
+function createTypeArray<T>(length: number, value: T): Array<T> {
     let arr: T[] = [];
-    for (let i: number = 0; i < length; i++){
+    for (let i: number = 0; i < length; i++) {
         arr.push(value);
     }
     return arr;
@@ -22,44 +22,57 @@ console.log(strArr);
 
 // 多个类型参数
 // 传入 [U,T] 返回 [T,U]
-function swap<U, T>(tuple: [U, T]): [T, U]{
+function swap<U, T>(tuple: [U, T]): [T, U] {
     return [tuple[1], tuple[0]];
 }
 
 // 泛型约束
 // 希望传入的参数有 length 属性,需要使用泛型约束
 // 定义约束类型
-interface LengthWise{
+interface LengthWise {
     length: number
 }
-function showLength<T extends LengthWise>(attr: T): void{
+function showLength<T extends LengthWise>(attr: T): void {
     console.log(attr.length);
 }
 showLength('aaaa');
-showLength([1,2,3,4]);
+showLength([1, 2, 3, 4]);
 // 传入没 length 属性的参数会报错
 // showLength(123);
 
 // 类型参数互相约束
 // T 继承了 U,使得 U 中不会包含 T 中没有的属性
-function copyFields<T extends U, U>(target: T, source: U): T{
-    for(let key in source){
+function copyFields<T extends U, U>(target: T, source: U): T {
+    for (let key in source) {
         target[key] = (<T>source)[key];
     }
     return target;
 }
-console.log(copyFields({a: 1, b: 2, c: 3}, {c: 4}));
+console.log(copyFields({ a: 1, b: 2, c: 3 }, { c: 4 }));
 // 有不存在的属性,报错
 // console.log(copyFields({ a: 1, b: 2, c: 3 }, { d: 4 }));
 
 // 使用 interface 和 泛型 定义函数形状
-interface ArrType{
+interface ArrType {
     <T>(length: number, value: T): Array<T>
 }
 let fillArray: ArrType;
-fillArray = function <T>(length: number, value: T): Array<T>{
+fillArray = function <T>(length: number, value: T): Array<T> {
     let arr: Array<T> = [];
-    for (let i: number = 0; i < length; i++){
+    for (let i: number = 0; i < length; i++) {
+        arr.push(<T>value);
+    }
+    return arr;
+}
+
+// 优化
+interface ArrType2<T> {
+    (length: number, value: T): Array<T>
+}
+let fillArray2: ArrType2<any>;
+fillArray2 = function <T>(length: number, value: T): Array<T> {
+    let arr: Array<T> = [];
+    for (let i: number = 0; i < length; i++) {
         arr.push(<T>value);
     }
     return arr;
