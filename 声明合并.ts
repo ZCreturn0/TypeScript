7b47f4ba2b3fb2edcc939c255c47f9366ee7a34a
ZCreturn0 <237350543@qq.com>
2019-10-26 20:16:10 +0800

    接口合并

--- 声明合并.ts
+++ 声明合并.ts
@@ -9,3 +9,16 @@ function reverse(x: number|string): number|string{
         return Number((<number>x).toString().split('').reverse().join(''));
     }
 }
+
+// 接口合并
+interface Alarm{
+    price: number;
+}
+interface Alarm{
+    weight: string;
+}
+// 相当于
+interface Alarm{
+    price: number;
+    weight: string;
+}
